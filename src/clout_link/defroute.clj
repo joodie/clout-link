7510d0c8be485de3633cfdce118fe0ed1e320a20
Joost Diepenmaat <joost@zeekat.nl>
2011-09-19 17:04:55 +0200

    more data about routes

--- src/clout_link/defroute.clj
+++ src/clout_link/defroute.clj
@@ -17,25 +17,27 @@
                                     :params (merge (:params request#) params#))]
                ~@body)))))))
 
-
 (defmacro defroute
   [n method spec & regexs]
   (let [p (parse-route spec) 
         args (map #(symbol (name %)) (filter keyword? p))
+        url-for-sym (symbol (str "url-for-" (name n)))
         route {:spec spec
                :method method
-               :parsed-route p
-               :regexs regexs
-               :args args}]
+               :parsed-route (list `quote p)
+               :regexs (list `quote regexs)
+               :args (list `quote args)}]
     `(do
-       (def ~n (quote ~route))
-       (defn ~(symbol (str "url-for-" (name n)))
+       (defn ~url-for-sym
          ~(vec args)
          (str ~@(insert-args p args)))
        (defmacro ~(symbol (str "handle-" (name n)))
          {:arglists '([~(vec args) & body] [request & body])}
          [~'bindings & ~'body]
          (make-handler-fn ~spec ~regexs
-                            ~method ~'bindings ~'body)))))
+                          ~method ~'bindings ~'body))
+       (def ~n (assoc ~route :url-fn ~url-for-sym)))))
+
+
 
 
