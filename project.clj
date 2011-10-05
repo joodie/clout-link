873a0bd4e42229af63d2cceb737cdc68ff6f108e
Joost Diepenmaat <joost@zeekat.nl>
2011-10-05 11:08:50 +0200

    update version and docs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clout-link "0.0.2-SNAPSHOT"
+(defproject clout-link "0.0.3"
   :description "Generate links based on clout route specs"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
